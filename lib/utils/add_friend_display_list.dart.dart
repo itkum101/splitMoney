15f96591cd3e960f32a849bbb90f650a0ee1cc5f
Amrit99999 <077bct009.amrit@pcampus.edu.np>
2023-01-08 17:32:17 +0545

    slow_contacts_fetch_issue_solved

--- lib/utils/add_friend_display_list.dart.dart
+++ lib/utils/add_friend_display_list.dart.dart
@@ -15,28 +15,33 @@ class AddFriendDisplayList extends StatefulWidget {
 }
 
 class _AddFriendDisplayListState extends State<AddFriendDisplayList> {
-   bool isLoading = true;
+  bool isLoading = true;
   bool noMatch = false;
   @override
   void initState() {
     super.initState();
     getContactPermission();
   }
+
   Future<void> getContactPermission() async {
-    if (await Permission.contacts.isGranted) {
+    if (await Permission.contacts.request().isGranted) {
+      //Either user already granted permissions earlier or the user just granted it now
       fetchContacts();
     } else {
-      await Permission.contacts.request();
+     await Permission.contacts.request();
+     
     }
   }
-void fetchContacts() async {
-    contactLists = await ContactsService.getContacts();
+
+  void fetchContacts() async {
+    contactLists = await ContactsService.getContacts(withThumbnails: false);
     setState(() {
       demoList = contactLists;
       isLoading = false;
     });
   }
-void updateList(String valuex) {
+
+  void updateList(String valuex) {
     setState(() {
       demoList = contactLists
           .where((element) => (element.displayName!
@@ -48,164 +53,157 @@ void updateList(String valuex) {
           .toList();
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Row(
-                children: [
-                  IconButton(
-                      color: Colors.grey[600],
-                      splashRadius: 20,
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: const Icon(Icons.arrow_back_rounded)),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Expanded(
-                    child: TextBox(
-                      onchangedFunc: (value) {
-                        updateList(value);
-                      },
-                      isautoFocus: true,
-                      hintText: "Enter name or phone no.",
-                      border: InputBorder.none,
-                      cursorColor: Colors.grey[600],
-                      cursorHeight: 21,
-                    ),
-                  )
-                ],
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            children: [
+              IconButton(
+                  color: Colors.grey[600],
+                  splashRadius: 20,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.arrow_back_rounded)),
+              const SizedBox(
+                width: 15,
               ),
-            ),
-            Container(
-              height: 1,
-              color: Colors.grey[400],
-            ),
-            const SizedBox(
-              height: 13,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return AddContact();
-                }));
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Row(children: [
-                  CircleAvatar(
-                    radius: 20,
-                    backgroundColor: Colors.green[100],
-                    child: Icon(
-                      Icons.person_add_outlined,
-                      color: Colors.grey[800],
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Expanded(
-                      child: Text(
-                    "Add a new Contact",
-                    style: TextStyle(
-                        color: Colors.grey[800],
-                        fontWeight: FontWeight.w500,
-                        fontSize: 16),
-                  ))
-                ]),
+              Expanded(
+                child: TextBox(
+                  onchangedFunc: (value) {
+                    updateList(value);
+                  },
+                  isautoFocus: true,
+                  hintText: "Enter name or phone no.",
+                  border: InputBorder.none,
+                  cursorColor: Colors.grey[600],
+                  cursorHeight: 21,
+                ),
+              )
+            ],
+          ),
+        ),
+        Container(
+          height: 1,
+          color: Colors.grey[400],
+        ),
+        const SizedBox(
+          height: 13,
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return const AddContact();
+            }));
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(children: [
+              CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.green[100],
+                child: Icon(
+                  Icons.person_add_outlined,
+                  color: Colors.grey[800],
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: Row(
-                children: [
-                  Text(
-                    "From your contacts",
-                    style: TextStyle(
-                        color: Colors.grey[800],
-                        fontWeight: FontWeight.w700,
-                        fontSize: 11),
-                  ),
-                ],
+              const SizedBox(
+                width: 15,
               ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Expanded(
-              child: isLoading
-                  ? const Center(child: CircularProgressIndicator())
-                  : ListView.builder(
-                      itemCount: demoList.length,
-                      itemBuilder: ((context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 15, vertical: 5),
-                          child: ListTile(
-                            onTap: (() {
-                              // context
-                              //     .read<FriendNameProvider>()
-                              //     .addToFriendList(FriendList(
-                              //       friendEmail:
-                                        // demoList[index].emails!.isNotEmpty
-                                        //     ? demoList[index].emails![0].value!
-                                        //     : "N/A",
-                              //       friendName: demoList[index].displayName!,
-                                    // imgChild: demoList[index].avatar!.isEmpty
-                                    //     ? const Icon(Icons.person)
-                                    //     : ClipOval(
-                                    //         child: Image.memory(
-                                    //           demoList[index].avatar!,
-                                    //           fit: BoxFit.fill,
-                                    //         ),
-                                    //       ),
-                              //     ));
-                              Provider.of<FriendNameProvider>(context,listen: false).addToFriendList(FriendList(friendName: demoList[index].displayName!,
-                               friendEmail: demoList[index].emails!.isNotEmpty
-                                            ? demoList[index].emails![0].value!
-                                            : "N/A", imgChild:demoList[index].avatar!.isEmpty
-                                        ? const Icon(Icons.person)
-                                        : ClipOval(
-                                            child: Image.memory(
-                                              demoList[index].avatar!,
-                                              fit: BoxFit.fill,
-                                            ),
-                                          ),));
+              Expanded(
+                  child: Text(
+                "Add a new Contact",
+                style: TextStyle(
+                    color: Colors.grey[800],
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16),
+              ))
+            ]),
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            children: [
+              Text(
+                "From your contacts",
+                style: TextStyle(
+                    color: Colors.grey[800],
+                    fontWeight: FontWeight.w700,
+                    fontSize: 11),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Expanded(
+          child: isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                  itemCount: demoList.length,
+                  itemBuilder: ((context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 5),
+                      child: ListTile(
+                        onTap: (() {
+                          // context
+                          //     .read<FriendNameProvider>()
+                          //     .addToFriendList(FriendList(
+                          //       friendEmail:
+                          // demoList[index].emails!.isNotEmpty
+                          //     ? demoList[index].emails![0].value!
+                          //     : "N/A",
+                          //       friendName: demoList[index].displayName!,
+                          // imgChild: demoList[index].avatar!.isEmpty
+                          //     ? const Icon(Icons.person)
+                          //     : ClipOval(
+                          //         child: Image.memory(
+                          //           demoList[index].avatar!,
+                          //           fit: BoxFit.fill,
+                          //         ),
+                          //       ),
+                          //     ));
+                          Provider.of<FriendNameProvider>(context,
+                                  listen: false)
+                              .addToFriendList(FriendList(
+                                  friendName: demoList[index].displayName!,
+                                  friendEmail:
+                                      demoList[index].emails!.isNotEmpty
+                                          ? demoList[index].emails![0].value!
+                                          : "N/A",
+                                  imgChild: const Icon(Icons.person)));
 
-                              Navigator.pop(context);
-                            }),
-                            leading: CircleAvatar(
-                                child: demoList[index].avatar!.isEmpty
-                                    ? const Icon(Icons.person)
-                                    : ClipOval(
-                                        child: Image.memory(
-                                          demoList[index].avatar!,
-                                          fit: BoxFit.fill,
-                                        ),
-                                      )),
-                            title: Text(
-                              demoList[index].displayName!,
-                              style: TextStyle(
-                                  color: Colors.grey[700],
-                                  fontWeight: FontWeight.w500),
-                            ),
-                            subtitle: Text(
-                              (demoList[index].phones!.isNotEmpty)
-                                  ? demoList[index].phones![0].value!
-                                  : "N/A",
-                            ),
-                          ),
-                        );
-                      })),
-            )
-          ],
-        );
+                          Navigator.pop(context);
+                        }),
+                        leading: const CircleAvatar(
+                          child: Icon(Icons.person),
+                        ),
+                        title: Text(
+                          demoList[index].displayName!,
+                          style: TextStyle(
+                              color: Colors.grey[700],
+                              fontWeight: FontWeight.w500),
+                        ),
+                        subtitle: Text(
+                          (demoList[index].phones!.isNotEmpty)
+                              ? demoList[index].phones![0].value!
+                              : "N/A",
+                        ),
+                      ),
+                    );
+                  })),
+        )
+      ],
+    );
   }
 }
